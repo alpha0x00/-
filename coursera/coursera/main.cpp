1696a7e6b7a6f49d4c0f7327c9e30aa9fb625073
Alpha0x00 <max@MaxLeedeMacBook-Pro.local>
2017-03-05 17:06:11 +0800

    第四周

---
+++ coursera/coursera/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursera
+//
+//  Created by Max on 2017/3/5.
+//  Copyright © 2017年 Alpha0x00. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ coursera/week4.cpp
@@ -0,0 +1,9 @@
+//
+//  week4.cpp
+//  Coursera
+//
+//  Created by Max on 2017/3/5.
+//  Copyright © 2017年 Alpha0x00. All rights reserved.
+//
+
+#include <stdio.h>
