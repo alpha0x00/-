4d842f08ea11f940e46e0aea727e3aace587b240
Alpha0x00 <max@MaxLeedeMacBook-Pro.local>
2017-03-09 09:56:36 +0800

    update

--- coursera/coursera/main.cpp
+++ coursera/coursera/main.cpp
@@ -179,8 +179,15 @@ int main()
 //        cout << "NO" << endl;
 //    }
     
-    
-    
+    int m,n,result = 0;
+    cin >> m >> n;
+    while (m<=n) {
+        if (m%2==1) {
+            result += m;
+        }
+        m++;
+    }
+    cout << result << endl;
     
     return 0;
 }
